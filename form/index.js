a895df39790e256916556f5f716a13527277fece
KIKonstantin <92094598+KIKonstantin@users.noreply.github.com>
2022-10-22 16:32:53 +0300

    Add files via upload

---
+++ form/index.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form-reg');
+const btn = document.getElementById('register-btn');
+const inputFields = document.querySelectorAll('input');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     alert('I work!');
+// })
